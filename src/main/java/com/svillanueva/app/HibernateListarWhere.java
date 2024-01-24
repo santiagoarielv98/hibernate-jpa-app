3efa66ca4ef4d9516ce359460de3a5bcbbe61717
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-24 15:35:56 -0300

    Clausula where hql

---
+++ src/main/java/com/svillanueva/app/HibernateListarWhere.java
@@ -0,0 +1,28 @@
+package com.svillanueva.app;
+
+import com.svillanueva.app.entity.Cliente;
+import com.svillanueva.app.utility.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+
+import java.util.Scanner;
+
+public class HibernateListarWhere {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        EntityManager entityManager = JpaUtil.getEntityManager();
+        Query query = entityManager.createQuery("SELECT c from Cliente c WHERE c.formaPago = ?1", Cliente.class);
+
+        System.out.println("Ingrese forma de pago: ");
+        String pago = scanner.next();
+
+        query.setParameter(1, pago);
+
+        Cliente c = (Cliente) query.getSingleResult();
+
+        System.out.println(c);
+
+        entityManager.close();
+    }
+}
---
+++ src/main/java/com/svillanueva/app/HibernatePorId2.java
@@ -0,0 +1,22 @@
+package com.svillanueva.app;
+
+import com.svillanueva.app.entity.Cliente;
+import com.svillanueva.app.utility.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+import java.util.Scanner;
+
+public class HibernatePorId2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        EntityManager entityManager = JpaUtil.getEntityManager();
+        System.out.println("Ingrese ID: ");
+        long id = scanner.nextLong();
+        Cliente c = entityManager.find(Cliente.class, id);
+
+        System.out.println(c);
+
+        entityManager.close();
+    }
+}
