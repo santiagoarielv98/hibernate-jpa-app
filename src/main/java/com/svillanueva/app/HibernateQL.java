f6319cc5352a39711816d233c526a50a57613e79
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-25 08:54:53 -0300

    hql

---
+++ src/main/java/com/svillanueva/app/HibernateQL.java
@@ -0,0 +1,54 @@
+package com.svillanueva.app;
+
+import com.svillanueva.app.entity.Cliente;
+import com.svillanueva.app.utility.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class HibernateQL {
+    public static void main(String[] args) {
+        EntityManager entityManager = JpaUtil.getEntityManager();
+        System.out.println("========== consultar todo ==========");
+        List<Cliente> clientes = entityManager.createQuery("SELECT c from Cliente c", Cliente.class)
+                .getResultList();
+        clientes.forEach(System.out::println);
+
+        System.out.println("========== consulta por id ==========");
+        Cliente cliente = entityManager.createQuery("SELECT c FROM Cliente c WHERE c.id=:idCliente", Cliente.class)
+                .setParameter("idCliente", 1L)
+                .getSingleResult();
+        System.out.println(cliente);
+
+        System.out.println("========== consulta solo el nombre por el id ==========");
+        String nombreCliente = entityManager.createQuery("SELECT c.nombre FROM Cliente c WHERE c.id=:id", String.class)
+                .setParameter("id", 1L)
+                .getSingleResult();
+        System.out.println(nombreCliente);
+
+        System.out.println("========== consulta por campos personalizados ==========");
+        Object[] clienteCamposPersonalizado = entityManager.createQuery("SELECT c.id, c.nombre, c.apellido FROM Cliente c WHERE c.id=:id", Object[].class)
+                .setParameter("id", 1L)
+                .getSingleResult();
+
+        Long id = (Long) clienteCamposPersonalizado[0];
+        String nombre = (String) clienteCamposPersonalizado[1];
+        String apellido = (String) clienteCamposPersonalizado[2];
+
+        System.out.println("id=" + id + ", nombre=" + nombre + ", apellido=" + apellido);
+
+        System.out.println("========== consulta por campos personalizados lista ==========");
+        List<Object[]> listaClienteCamposPersonalizado = entityManager.createQuery("SELECT c.id, c.nombre, c.apellido FROM Cliente c", Object[].class)
+                .getResultList();
+
+        listaClienteCamposPersonalizado.forEach(reg -> {
+            Long idCli = (Long) reg[0];
+            String nombreCli = (String) reg[1];
+            String apellidoCli = (String) reg[2];
+            System.out.println("id=" + idCli + ", nombre=" + nombreCli + ", apellido=" + apellidoCli);
+
+        });
+
+        entityManager.close();
+    }
+}
