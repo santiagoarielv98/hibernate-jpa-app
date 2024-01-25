a1150f8316bd47ec5266d09da92ebf5630f4c7bc
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-25 11:05:35 -0300

    mas ejemplo de criteria

--- src/main/java/com/svillanueva/app/HibernateCriteria.java
+++ src/main/java/com/svillanueva/app/HibernateCriteria.java
@@ -37,6 +37,34 @@ public class HibernateCriteria {
                 .getResultList();
         clientes.forEach(System.out::println);
 
+        System.out.println("========= usando where like para buscar clientes por nombre =========");
+        query = criteriaBuilder.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        ParameterExpression<String> nombreParamLike = criteriaBuilder.parameter(String.class, "nombreParam");
+
+//        query.where(criteriaBuilder.like(from.get("nombre"), "%tia%"));
+        query.select(from)
+//                .where(criteriaBuilder.like(from.get("nombre"), nombreParamLike));
+                .where(criteriaBuilder.like(criteriaBuilder.upper(from.get("nombre")), criteriaBuilder.upper(nombreParamLike)));
+
+        clientes = entityManager.createQuery(query)
+                .setParameter("nombreParam", "%ago%")
+                .getResultList();
+        clientes.forEach(System.out::println);
+
+
+        System.out.println("========= ejemplo usando where between para rangos =========");
+
+        query = criteriaBuilder.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+
+        query.select(from)
+                .where(criteriaBuilder.between(from.get("id"), 2L, 6L));
+        clientes = entityManager.createQuery(query)
+                .getResultList();
+        clientes.forEach(System.out::println);
+
+
         entityManager.close();
     }
 }
